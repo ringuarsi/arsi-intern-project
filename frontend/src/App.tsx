b6824a18055af007884903553956b735d2dc35bd
samuel <samuellmpachuau@gmail.com>
2026-06-29 07:00:00 +0000

    Add package-lock

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function App() {
   return (
     <div className="min-h-screen">
-      <main className="">
+      <main className="bg-red-400">
         <Outlet />
       </main>
     </div>
